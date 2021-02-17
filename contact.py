a11fdf70909d0f3ad59587ff852cfccf78fc6345
Mayank Sharma <205119053@nitt.edu>
2021-02-17 12:48:42 +0530

    Added contact form template

--- contact.py
+++ contact.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return '<h1>Hello</h1>'
+    return render_template('contact.html')
