d4d6ebfd81a643a8e72589413e8ca3ab539aac68
Mayank Sharma <205119053@nitt.edu>
2021-02-17 19:25:23 +0530

    Mail Functionality added

--- contact.py
+++ contact.py
@@ -5,6 +5,8 @@ from wtforms.validators import DataRequired,Email,Length
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
+from threading import Thread
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -18,9 +20,34 @@ app.config['SECRET_KEY'] = 'hard to guess'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+app.config['MAIL_SUBJECT_PREFIX'] = '[Query]'
+app.config['MAIL_SENDER'] = 'Admin <mayank.nimcet.188@gmail.com>'
+
+app.config['ADMIN'] = os.environ.get('ADMIN')
+
+
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
+
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to,subject,template,**kwargs):
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + subject,sender=app.config['MAIL_SENDER'],recipients=[to])
+    msg.body = render_template(template + '.txt',**kwargs)
+    msg.html = render_template(template + '.html',**kwargs)
+    thr = Thread(target=send_async_email,args=[app,msg])
+    thr.start()
+    return thr
 
 class QueryForm(FlaskForm):
     name = StringField('Name',validators=[DataRequired()])
@@ -46,9 +73,13 @@ def index():
         new_query = Query(name=form.name.data,email=form.email.data,subject=form.subject.data,query=form.query.data)
         db.session.add(new_query)
         db.session.commit()
-        flash('Your query has been submitted')
+        if app.config['ADMIN']:
+            send_email(app.config['ADMIN'], 'New Query Posted', 'mail/new_query',query=new_query)
+            flash('Your query has been submitted')
         return redirect(url_for('index'))
     return render_template('contact.html',form=form)
+
+
 
 @app.shell_context_processor
 def make_shell_context():
